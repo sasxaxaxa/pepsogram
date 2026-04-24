18a5aff36031d6f677aff624f2b6f2f73981713a
simsa <simsaaav@mail.ru>
2026-04-24 20:50:42 +0300

    файлики

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './styles/main.scss'
+
+
+function App() {
+  return (
+    <>
+      <section>
+        <div className="hero">
+          <h1>pepsik</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
